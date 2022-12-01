4303ff7cf3bf47234faaf55208846e5d67446acc
Alexander Ekhov <alex-vale@mail.ru>
2022-12-01 15:24:14 +0300

    Решения задач

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -12,7 +12,17 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+# Решение
+counter = {}
+print('Задание 1:')
+from collections import Counter
+list = []
+for student in students:
+    list.append(student['first_name'])
+counter = Counter(list)
+for name in counter:
+    print(f'{name}: {counter[name]}')
+
 
 
 # Задание 2
@@ -26,8 +36,24 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
+# Решение
+print('\nЗадание 2:')
+list = []
+counter = {}
+for student in students:
+    list.append(student['first_name'])     
+  
+counter = Counter(list)
 
+c = 0
+most_freq = ''
+for name, count in counter.items():
+    if count > c:
+        c = count
+        most_freq = name
+
+print(f'Самое частое имя среди учеников: {most_freq}')
+    
 
 # Задание 3
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
@@ -51,8 +77,25 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+# Решение
 
+print('\nЗадание 3:')
+class_number = 0
+for clas in school_students:
+    list = []
+    class_number += 1
+    for student in clas:
+        list.append(student['first_name'])     
+    
+    counter = Counter(list)
+    
+    c = 0
+    most_freq = ''
+    for name, count in counter.items():
+        if count > c:
+            c = count
+            most_freq = name
+    print(f'Самое частое имя среди учеников в классе {class_number}: {most_freq}')
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
@@ -63,7 +106,7 @@ school_students = [
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
     {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-    {'class': '2б', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
+    {'class': '2в', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]}, # Исправил класс повтор 2б на 2в.
 ]
 is_male = {
     'Олег': True,
@@ -72,7 +115,20 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
+# Решение
+print('\nЗадание 4:')
+for clas in school:
+    male = 0
+    female = 0
+    gender = bool
+    for name in clas['students']:
+        if name['first_name'] in is_male:
+            gender = is_male[name['first_name'] ]
+            if gender == True:
+                male += 1
+            else:
+                female += 1
+    print(f"Класс {clas['class']}: девочки {female}, мальчики {male}")
 
 
 # Задание 5
@@ -91,5 +147,22 @@ is_male = {
     'Олег': True,
     'Миша': True,
 }
-# ???
-
+# Решение
+print('\nЗадание 5:')
+for clas in school:
+    male = 0
+    female = 0
+    gender = bool
+    gender_most = ""
+    for name in clas['students']:
+        if name['first_name'] in is_male:
+            gender = is_male[name['first_name'] ]
+            if gender == True:
+                male += 1
+            else:
+                female += 1
+    if male > female:
+        gender_most = "мальчиков"
+    else:
+        gender_most = "девочек"
+    print(f"В классе {clas['class']} больше всего {gender_most}.")
--- string_challenges.py
+++ string_challenges.py
@@ -2,27 +2,67 @@
 word = 'Архангельск'
 # ???
 
+print('Задание: Вывести последнюю букву в слове:')
+letters = list(word)
+print(letters[-1])
+
 
 # Вывести количество букв "а" в слове
+print('Вывести количество букв "а" в слове:')
 word = 'Архангельск'
 # ???
 
+c = 0
+for letter in word:
+    if letter.capitalize() == "А":
+        c += 1
+
+print(c)
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
 # ???
+
+print('Вывести количество гласных букв в слове:')
+alphabet = ['А', 'О', 'У', 'Ы', 'Э', 'Е', 'Ё', 'И', 'Ю', 'Я']
+c = 0
+for letter in word:
+    if letter.capitalize() in alphabet:
+        c += 1
+print(c)    
 
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
 # ???
 
+print('Вывести количество слов в предложении:')
+words = sentence.split()
+print(len(words))
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
 # ???
 
+print('Вывести первую букву каждого слова на отдельной строке:')
+words = sentence.split()
+for word in words:
+    letters = list(word)
+    print(letters[0])
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
 # ???
+
+print('Вывести усреднённую длину слова в предложении:')
+words = sentence.split()
+letter_total = 0
+for word in words:
+    letter_total += len(word)
+print(letter_total/len(words))
+
+# Другой вариант решения.
+
+words = sentence.split()
+letters = sentence.replace(" ", '')
+print(len(letters)/len(words))
